94c0137a713cc3d7438e8a73eb3563a77ee0383b
eugoo1794 <8923107@ed.tus.ac.jp>
2026-06-02 10:31:06 +0900

    簡易実装

--- storage.js
+++ storage.js
@@ -3,12 +3,12 @@
 const STORAGE_KEY = "tasks";
 
 // 取得
-export function loadTasks() {
+function loadTasks() {
   const data = localStorage.getItem(STORAGE_KEY);
   return data ? JSON.parse(data) : [];
 }
 
 // 保存
-export function saveTasks(tasks) {
+function saveTasks(tasks) {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
 }
